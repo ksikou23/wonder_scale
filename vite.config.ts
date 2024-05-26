7e33037d774dabca5e536d4f9af80a2e8561d63e
ksikou23 <ksikou23@gmail.com>
2024-05-27 00:21:04 +0900

    feat(pwa): enable pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Wonder Scale",
+        short_name: "Wonder Scale",
+        theme_color: "#000000",
+      },
+    }),
+  ],
+});
