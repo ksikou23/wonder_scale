9075ca9302afa98f868fd1ecee1329f9c4ff030f
ksikou23 <ksikou23@gmail.com>
2024-06-06 03:07:13 +0900

    feat(position): add ipad pro 12.9inch positions to hardcoded values

--- src/util/position.ts
+++ src/util/position.ts
@@ -6,6 +6,14 @@ const position_ipad_pro_13inch = {
   visual: { x: 463, y: 2375 },
 };
 
+const position_ipad_pro_12dot9inch = {
+  width: 2048,
+  height: 2732,
+  vocal: { x: 460, y: 2024 },
+  dance: { x: 460, y: 2190 },
+  visual: { x: 460, y: 2357 },
+};
+
 const position_iphone_se2 = {
   width: 750,
   height: 1334,
@@ -14,7 +22,11 @@ const position_iphone_se2 = {
   visual: { x: 92, y: 1166 },
 };
 
-const positions = [position_ipad_pro_13inch, position_iphone_se2];
+const positions = [
+  position_ipad_pro_13inch,
+  position_ipad_pro_12dot9inch,
+  position_iphone_se2,
+];
 
 export default function get_hard_coded_position(width: number, height: number) {
   for (const position of positions) {
